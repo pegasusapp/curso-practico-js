dad737dd45bd9c6a49aef74fe1e9009efe74f4d9
Oscar <rsi.dev6@uis.edu.co>
2021-07-18 22:35:29 -0500

    feature(add html): add html page

--- figuras.js
+++ figuras.js
@@ -1,1 +1,8 @@
-console.log("Hola");
+console.group();
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: "+ladoCuadrado);
+const perimetroCuadrado = ladoCuadrado*4;
+console.log("El perimetro del cuadrado es: "+perimetroCuadrado);
+const areaCuadrado = ladoCuadrado*ladoCuadrado;
+console.log("El area del cuadrado es: "+ areaCuadrado+" cms");
+console.groupEnd();
